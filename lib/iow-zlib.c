e444885283d54379a9b7179d0236c77b288e4cd3
Robert A. Zeh <robert.a.zeh@gmail.com>
2017-05-24 09:15:42 -0500

    Comment Fix
    The write modules compress bytes, they do not decompress them.

--- lib/iow-zlib.c
+++ lib/iow-zlib.c
@@ -134,7 +134,7 @@ static int64_t zlib_wwrite(iow_t *iow, const char *buffer, int64_t len)
 				DATA(iow)->err = ERR_ERROR;
 		}
 	}
-	/* Return the number of bytes decompressed */
+	/* Return the number of bytes compressed */
 	return len-DATA(iow)->strm.avail_in;
 }
 
