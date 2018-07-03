651ff2f37fd7ab464f22e0860e62cf6dc6e489a0
Brad Cowie <brad@wand.net.nz>
2018-07-03 14:30:15 +1200

    It's better to build individual debs for each suite.

---
+++ gitlab-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x -e -o pipefail
+
+export DEBEMAIL='packaging@wand.net.nz'
+export DEBFULLNAME='WAND Packaging'
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get install -y equivs devscripts dpkg-dev quilt curl apt-transport-https \
+    apt-utils ssl-cert ca-certificates gnupg lsb-release debhelper git
+apt-get upgrade -y
+
+debchange --newversion ${CI_COMMIT_REF_NAME} -b "New upstream release"
+mk-build-deps -i -r -t 'apt-get -f -y --force-yes'
+dpkg-buildpackage -b -us -uc -rfakeroot -j4
