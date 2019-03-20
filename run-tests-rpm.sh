e23b15f7b569bfe4ebca5f3a79e8f3839f7e0f30
Shane Alcock <salcock@waikato.ac.nz>
2019-03-20 15:18:29 +1300

    Remove amd64/ directory from test running scripts

--- run-tests-rpm.sh
+++ run-tests-rpm.sh
@@ -13,9 +13,9 @@ fi
 
 yum install -y xz zstd gzip bzip2 lzop lz4
 
-yum install -y built-packages/amd64/$1/libwandio1-${CI_COMMIT_REF_NAME}-*.rpm
-yum install -y built-packages/amd64/$1/libwandio1-devel-${CI_COMMIT_REF_NAME}-*.rpm
-yum install -y built-packages/amd64/$1/libwandio1-tools-${CI_COMMIT_REF_NAME}-*.rpm
+yum install -y built-packages/$1/libwandio1-${CI_COMMIT_REF_NAME}-*.rpm
+yum install -y built-packages/$1/libwandio1-devel-${CI_COMMIT_REF_NAME}-*.rpm
+yum install -y built-packages/$1/libwandio1-tools-${CI_COMMIT_REF_NAME}-*.rpm
 
 cd test && ./do-basic-tests.sh
 
--- run-tests.sh
+++ run-tests.sh
@@ -10,9 +10,7 @@ apt-get upgrade -y
 
 echo "deb https://dl.bintray.com/wand/general $(lsb_release -sc) main" | tee -a /etc/apt/sources.list.d/wand.list
 
-curl --silent "https://bintray.com/user/downloadSubjectPublicKey?username=wand" | gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/wand.gpg --import
-
-chmod 644 /etc/apt/trusted.gpg.d/wand.gpg
+curl --silent "https://bintray.com/user/downloadSubjectPublicKey?username=wand" | apt-key add -
 
 apt-get update
 
@@ -21,14 +19,12 @@ apt-get install -y zstd lzop xz-utils liblz4-tool
 
 mk-build-deps --install --remove --tool "apt-get -o Debug::pkgProblemResolver=yes -y --no-install-recommends"
 
-deb=`find built-packages/amd64/$1/libwandio1-dev* -maxdepth 1 -type f`
+deb=`find built-packages/$1/libwandio1-dev* -maxdepth 1 -type f`
 pkg_filename=$(basename "${deb}")
 IFS=_ read pkg_name pkg_version pkg_arch <<< $(basename -s ".deb" "${pkg_filename}")
 
-dpkg -i built-packages/amd64/$1/libwandio1_${pkg_version}_${pkg_arch}.deb
-dpkg -i built-packages/amd64/$1/libwandio1-dev_${pkg_version}_${pkg_arch}.deb
-dpkg -i built-packages/amd64/$1/wandio1-tools_${pkg_version}_${pkg_arch}.deb
+dpkg -i built-packages/$1/libwandio1_${pkg_version}_${pkg_arch}.deb
+dpkg -i built-packages/$1/libwandio1-dev_${pkg_version}_${pkg_arch}.deb
+dpkg -i built-packages/$1/wandio1-tools_${pkg_version}_${pkg_arch}.deb
 
-#./bootstrap.sh && ./configure && make
 cd test && ./do-basic-tests.sh
-
