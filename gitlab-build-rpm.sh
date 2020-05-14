e20a19db8685fc407f42314b5140d7b5cf28ce44
Shane Alcock <salcock@waikato.ac.nz>
2020-05-15 10:53:28 +1200

    Fix issues with missing libzstd packages on Centos 8

--- gitlab-build-rpm.sh
+++ gitlab-build-rpm.sh
@@ -7,6 +7,16 @@ SOURCENAME=`echo ${CI_COMMIT_REF_NAME} | cut -d '-' -f 1`
 
 if [ "$1" = "centos8" ]; then
         yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+
+        dnf install -y 'dnf-command(config-manager)' || true
+        yum config-manager --set-enabled PowerTools || true
+
+        # XXX Temporary, until Centos updates to 8.2 where libzstd is
+        # included in the base OS
+        # ref: https://lists.fedoraproject.org/archives/list/epel-devel@lists.fedoraproject.org/thread/MFZCRQCULJALRIJJFSSAETSDZ4RL6GCU/
+        wget -N https://archives.fedoraproject.org/pub/archive/epel/8.1/Everything/x86_64/Packages/l/libzstd-1.4.4-1.el8.x86_64.rpm && rpm -i libzstd-1.4.4-1.el8.x86_64.rpm
+
+        wget -N https://archives.fedoraproject.org/pub/archive/epel/8.1/Everything/x86_64/Packages/l/libzstd-devel-1.4.4-1.el8.x86_64.rpm && rpm -i libzstd-devel-1.4.4-1.el8.x86_64.rpm
 fi
 
 if [ "$1" = "centos7" ]; then
