b0ad050e111700de1a1a5044ad43b782f7b9aa0c
Shane Alcock <salcock@waikato.ac.nz>
2020-05-28 17:37:48 +1200

    Fix copy-paste error when borrowing scripts from libtrace :/

--- rpmpkg-setup.sh
+++ rpmpkg-setup.sh
@@ -50,11 +50,11 @@ fi
 if [[ "$1" =~ fedora* ]]; then
         dnf install -y rpm-build rpmdevtools 'dnf-command(builddep)' which
         dnf group install -y "C Development Tools and Libraries"
-        dnf builddep -y rpm/libtrace4.spec
+        dnf builddep -y rpm/libwandio1.spec
 else
         yum install -y rpm-build yum-utils rpmdevtools which
         yum groupinstall -y 'Development Tools'
-        yum-builddep -y rpm/libtrace4.spec
+        yum-builddep -y rpm/libwandio1.spec
 fi
 
 rpmdev-setuptree
