f6385fb8d4eb40a650e0e38fadabe52fbf523ac2
Shane Alcock <salcock@waikato.ac.nz>
2020-05-29 09:33:16 +1200

    Add post-build tests to github action CI
    Updated test scripts to match behaviour of github CI, e.g. looking
    for built packages in packages/ instead of built-packages/.

--- run-tests-rpm.sh
+++ run-tests-rpm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -x -e -o pipefail
-SOURCENAME=`echo ${CI_COMMIT_REF_NAME} | cut -d '-' -f 1`
+SOURCENAME=`echo ${GITHUB_REF##*/} | cut -d '-' -f 1`
+
 ZSTDREQ=zstd
 
 if [ "$1" = "centos_8" ]; then
@@ -27,16 +28,11 @@ if [ "$1" = "centos_7" ]; then
         yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 fi
 
-if [ "$1" = "centos_6" ]; then
-        yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-        yum install -y epel-rpm-macros
-fi
-
 yum install -y xz ${ZSTDREQ} gzip bzip2 lzop lz4 diffutils
 
-yum install -y built-packages/$1/libwandio1-${SOURCENAME}-*.rpm
-yum install -y built-packages/$1/libwandio1-devel-${SOURCENAME}-*.rpm
-yum install -y built-packages/$1/libwandio1-tools-${SOURCENAME}-*.rpm
+yum install -y packages/$1/libwandio1-${SOURCENAME}-*.rpm
+yum install -y packages/$1/libwandio1-devel-${SOURCENAME}-*.rpm
+yum install -y packages/$1/libwandio1-tools-${SOURCENAME}-*.rpm
 
 cd test && ./do-basic-tests.sh
 
