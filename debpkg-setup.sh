0f4cacf60ed6a19948722176e614b99619a16057
Shane Alcock <shane@alcock.co.nz>
2022-10-21 14:22:52 +1300

    Deal with lack of debhelper 12 in default bionic repository

--- debpkg-setup.sh
+++ debpkg-setup.sh
@@ -8,6 +8,8 @@ export DEBIAN_FRONTEND=noninteractive
 
 export SOURCENAME=`echo ${GITHUB_REF##*/} | cut -d '-' -f 1`
 
+LSB=`lsb_release -cs`
+
 apt-get update
 apt-get install -y equivs devscripts dpkg-dev quilt curl apt-transport-https \
     apt-utils ssl-cert ca-certificates gnupg lsb-release debhelper git \
@@ -17,4 +19,9 @@ curl -1sLf 'https://dl.cloudsmith.io/public/wand/libwandio/cfg/setup/bash.deb.sh
 
 apt-get update
 apt-get upgrade -y
+
+if [ "${LSB}" == "bionic" ]; then
+    apt install -y debhelper/${LSB}-backports
+fi
+
 
