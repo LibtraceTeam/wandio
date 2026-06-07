2118406ed7f1e5c3911487d370bdfdd1c9375ea4
scherepanov <sergey@cherepanov.org>
2026-06-07 08:32:23 -0400

    Fix error messages for ZSTD/LZ4 frame handling

--- lib/ior-zstd-lz4.c
+++ lib/ior-zstd-lz4.c
@@ -174,13 +174,13 @@ static int64_t zstd_lz4_read(io_t *io, void *buffer, int64_t len) {
 
                 while (true) {
                         if (DATA(io)->dec == DEC_UNDEF) {
-                                if (len < 4) {
-                                        fprintf(stderr, "Invalid too short "
-                                                        "ZSTD/LJS frame\n");
-                                        errno = EIO;
-                                        return -1;
-                                }
-                                unsigned char *buf =
+                            int avail = DATA(io)->inbuf_len - DATA(io)->inbuf_index;
+                            if(avail < 4) {
+                                  /* Not enough bytes to identify the next frame. Break to the
+                                   * outer loop to refill the input buffer. */
+                                  break;                                
+                            }
+                            unsigned char *buf =
                                     (unsigned char *)(DATA(io)->inbuf +
                                                       DATA(io)->inbuf_index);
                                 if (((buf[0] & 0xf0) == 0x50) &&
@@ -203,7 +203,7 @@ static int64_t zstd_lz4_read(io_t *io, void *buffer, int64_t len) {
 #endif
                                 } else {
                                         fprintf(stderr,
-                                                "Unknown ZSTD/LJS frame \n");
+                                                "Unknown ZSTD/LZ4 frame \n");
                                         DATA(io)->dec = DEC_UNDEF;
                                         errno = EIO;
                                         return -1;
